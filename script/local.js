bf9f0cb473accdfaddaa7f84f1bb4889b4487e56
loveyuzhongkai <2578218307@qq.com>
2016-04-16 19:12:50 +0800

    test

--- script/local.js
+++ script/local.js
@@ -12,43 +12,49 @@ $(function () {
         setInterval(autoPlay());
     });
     //-------------回车键事件
-    document.onkeydown = function(event_e){
-        if(window.event)
+    document.onkeydown = function (event_e) {
+        if (window.event)
             event_e = window.event;
-            var int_keycode = event_e.charCode||event_e.keyCode;
-            if(int_keycode ==13){
-                $('.loader').show();
+        var int_keycode = event_e.charCode || event_e.keyCode;
+        if (int_keycode == 13) {
+            var search_v = $(".search").val().trim();
+            var reg = /^[\u4e00-\u9fa5]{0,}$/;
+            if (reg.test(search_v)) {
                 $('.opacity_bg').show();
-                var v = $(".search").val();
-                var url= "search.html"+"?"+"c="+v;
+                $('.loader').show();
+                var url = "search.html" + "?" + "c=" + search_v;
                 setTimeout(function () {
                     location.href = encodeURI(url);
-                },3000);
+                }, 3000);
+            } else {
+                alert("请输入中文");
+                return;
+            }
         }
     }
 
     //-------------屏幕滚动事件
-    $(window).scroll(function () {
-      var m = $(document).scrollTop();
+    $(document).scroll(function () {
+        var m = $(document).scrollTop();
         //document.title = m;
-        if(m>300){
+        if (m > 300) {
             $(".nav").fadeIn(500);
-        }else{
+        } else {
             $(".nav").fadeOut(500);
         }
-        if(m>800){
+        if (m > 800) {
             $(".back_top").fadeIn(300);
-        }else{
+        } else {
             $(".back_top").fadeOut(800);
         }
     })
 
     //-------------logo点击事件
-    $(document).on("click",".nav_left", function () {
+    $(document).on("click", ".nav_left", function () {
         location.href = "offer.html";
     });
     //-------------返回顶部事件
-    $(document).on("click",".back_top", function () {
-        $("html,body").animate({"scrollTop": 0}, 400);
+    $(document).on("click", ".back_top", function () {
+        $("html,body").animate({"scrollTop": 0}, 300);
     })
 })
